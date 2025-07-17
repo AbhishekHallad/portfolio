de7cf9a94ea3633e97ea498db48bb9ef918b312b
Abhishek Hallad <abhishekhallad@dhcp-10-8-000-151.mobile.reshsg.uci.edu>
2025-07-17 01:47:03 -0700

    updating paths

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar.jsx'
 import Home from './pages/Home'
 import { useState, useEffect } from 'react'
 import { FaHistory, FaLightbulb, FaRocket, FaCode, FaPaintBrush, FaTools, FaGithub, FaExternalLinkAlt, FaSun, FaMoon, FaVolumeUp, FaVolumeMute, FaDatabase, FaBrain } from 'react-icons/fa'
